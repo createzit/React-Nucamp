b7f48ef1994d38eb2c769e01833d119111e7543a
Linh Nguyen <nguyenthuonglinh@gmail.com>
2020-03-27 10:34:24 -0700

    React Router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
@@ -36,6 +37,14 @@ class Main extends Component {
 			);
 		}
 
+		const CampsiteWithId = ({ match }) => {
+			return (
+				<CampsiteInfo
+					campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+					comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+				/>
+			);
+		}
 		return (
 			<div>
 				<Header />
@@ -43,6 +52,7 @@ class Main extends Component {
 					<Route path='/home' component={HomePage} />
 					<Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
 					<Route exact path='/contactus' component={Contact} />
+					<Route exact path='/directory/:campsiteId' component={CampsiteWithId}/>
 					<Redirect to='/home'/>
 				</Switch>
 				<Footer />
