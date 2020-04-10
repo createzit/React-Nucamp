6899468914ff4a26820a9259a63ce2e3410cbe42
Linh Nguyen <nguyenthuonglinh@gmail.com>
2020-04-10 11:48:17 -0700

    combining Reducers

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,90 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, {Component} from 'react';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem,Button, Modal,ModalHeader, Label, ModalBody } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+
+class CommentForm extends Component{
+
+	constructor(props){
+		super(props);
+
+		this.state = {
+			isModalOpen: false
+		}
+
+	}
+
+	toggleModal = () => {
+		this.setState({
+			isModalOpen: !this.state.isModalOpen
+		});
+	}
+
+	handleSubmit(values) {
+		console.log('Current state is: ' + JSON.stringify(values));
+		alert('Current state is: ' + JSON.stringify(values));
+}
+
+	render() {
+		return (
+			<div>
+				<Button outline onClick= {this.toggleModal}>
+					<i className="fa fa-pencil fa-lg" /> Submit Comment
+				</Button>
+
+				<Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+					<ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader><hr/>
+					<ModalBody>
+						<LocalForm onSubmit={values => this.handleSubmit(values)}>
+								<div className="form-group">
+									<Label htmlFor="rating">Rating</Label><br/>
+									<Control.select model='.rating' name='rating' id='rating' className='form-control' defaultValue='1'>
+										<option>1</option>
+										<option>2</option>
+										<option>3</option>
+										<option>4</option>
+										<option>5</option>
+									</Control.select>
+								</div>
+								
+								<div className="form-group">
+									<Label htmlFor="author">Your Name</Label><br/>
+									<Control.text placeholder='Your Name' model='.author' name='author' id='author' className='form-control'
+										validators={{
+											minLength: minLength(2),
+											maxLength: maxLength(15)
+										}} />
+									<Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            minLength: 'Must be at least 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+								</div>
+
+								<div className="form-group">
+									<Label htmlFor="comment">Comment</Label><br/>
+									<Control.textarea placeholder='comment' model='.text' name='text' id='text' rows ='6'className='form-control' defaultChecked='No comment was entered'/>
+								</div>
+							<Button type="submit" value="submit" color="primary">
+								Submit 
+							</Button>
+						
+					</LocalForm>
+				</ModalBody>
+				</Modal>
+			</div>
+		)
+	}
+	
+}
 
 function RenderCampsite({ campsite }) {
 	return (
@@ -31,6 +115,7 @@ function RenderComments({ comments }) {
 							day: '2-digit'
 						}).format(new Date(Date.parse(comment.date)))}
 					</p>
+					
 				</div>
 			);
 		});
@@ -38,8 +123,10 @@ function RenderComments({ comments }) {
 			<div className="col-md-5 m-1">
 				<h4>Comments</h4>
 				{finalComments}
+				<CommentForm/>
 			</div>
 		);
+
 	}
 	return <div />;
 }
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,17 @@
-import { createStore } from 'redux';
-import { Reducer, initalState } from './reducer';
+import { createStore, combineReducers} from 'redux';
+import { Campsites } from './campsites';
+import { Comments } from './comments';
+import { Partners } from './partners';
+import {Promotions} from './promotions'
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer,
-        initalState
+        combineReducers({
+            campsites: Campsites,
+            comments: Comments,
+            partners: Partners,
+            promotions: Promotions
+        })
     );
     return store;
 }
