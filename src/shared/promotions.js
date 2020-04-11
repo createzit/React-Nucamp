076051df1faf5e15a34534e94f0c505b9a1c16bc
Linh Nguyen <nguyenthuonglinh@gmail.com>
2020-04-11 08:48:55 -0700

    Fetch Post Comment

--- src/shared/promotions.js
+++ src/shared/promotions.js
@@ -1,5 +1,6 @@
-import * as ActionTypes from './ActionTypes';
-export const Promotions = (
+import * as ActionTypes from '../redux/ActionTypes';
+
+export const PROMOTIONS = (
 	state = {
 		isLoading: true,
 		errMess: null,
